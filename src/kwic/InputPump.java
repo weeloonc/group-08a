e5310204f75256be74eaf2785900db8ede287163
Wee Loon <weeloonc@outlook.com>
2014-08-28 01:36:05 +0800

    Added InputPump class and OutputSink class

---
+++ src/kwic/InputPump.java
@@ -0,0 +1,15 @@
+package kwic;
+
+public class InputPump implements Runnable {
+	
+	public InputPump(Pipe output) {
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/kwic/OutputSink.java
@@ -0,0 +1,15 @@
+package kwic;
+
+public class OutputSink implements Runnable {
+	
+	public OutputSink(Pipe input) {
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
