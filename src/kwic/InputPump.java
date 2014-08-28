2ad1c7b818686390aa71071cff27001a0dc0304a
Wee Loon <weeloonc@outlook.com>
2014-08-28 16:55:32 +0800

    Updated InputPump class

--- src/kwic/InputPump.java
+++ src/kwic/InputPump.java
@@ -1,15 +1,41 @@
 package kwic;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class InputPump implements Runnable {
 	
+	private volatile boolean isRunning;
+	private Pipe output;
+	
 	public InputPump(Pipe output) {
-		
+		this.output = output;
 	}
 
 	@Override
-	public void run() {
-		// TODO Auto-generated method stub
+	public final void run() {
+		isRunning = true;
+		readInput();
+	}
+	
+	public final void interrupt() {
+		isRunning = false;
+	}
+	
+	private void readInput() {
 		
+		Scanner sc = new Scanner(System.in);
+		String line = sc.nextLine();
+		
+		while (line != "") {
+			try {
+				output.writeLine(line);
+				line = sc.nextLine();
+			} catch (IOException ex) {
+				// TODO Auto-generated catch block
+				ex.printStackTrace();
+			}
+		}
 	}
 
 }
