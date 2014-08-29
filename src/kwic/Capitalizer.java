c3b2497f61c4d3c7ed79e30787a3c4a06eadfc3f
Tan Hwa Foo <hwafoo@gmail.com>
2014-08-30 02:19:37 +0800

    Minor edit but still cannot work

--- src/kwic/Capitalizer.java
+++ src/kwic/Capitalizer.java
@@ -25,18 +25,7 @@ public class Capitalizer extends Filter {
 			//String[] words = line.split("\\s");  // spliting line with whitespaces
 			
 			//make the first letter upper case
-			System.out.println(line);
 			line = line.substring(0, 1).toUpperCase() + line.substring(1).toLowerCase();
-			
-			//loop through the words and make all lower case
-//			StringBuilder processedWord = new StringBuilder();
-//			processedWord.append(words[0]);
-//			for (int i = 1; i < words.length; i++){
-//				words[i].toLowerCase();
-//				processedWord.append(words[i]);
-//			}
-			System.out.println(line);
-//			int endIndex = processedWord.length() - 1;  // trim last space
 			getOutput().writeLine(line);
 			
 		} catch (IOException e) {
